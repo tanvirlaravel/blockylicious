4e724ed6f6f50d31957c5777034e7d6d145ddd00
Tanvir <tanvirlaravel@gmail.com>
2024-10-08 12:01:21 +0600

    some modification  in curve component

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '29a2c90885ad8d00e060');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8a874ee9ac5e1f57a80a');
