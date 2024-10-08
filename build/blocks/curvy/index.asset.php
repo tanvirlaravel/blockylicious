06093fc1087a0127de665a490cf1773363afae93
Tanvir <tanvirlaravel@gmail.com>
2024-10-08 10:39:47 +0600

    savg added in curvy component

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e37b9161f73a370038cf');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '60e5d1b4e58bf8a52e63');
