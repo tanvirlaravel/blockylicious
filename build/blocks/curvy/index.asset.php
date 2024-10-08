c142140db999a4100c3f2acecda8b3a8c57dbb56
Tanvir <tanvirlaravel@gmail.com>
2024-10-08 12:12:37 +0600

    set default padding

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8a874ee9ac5e1f57a80a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e5607e5cb92a0cb0bf32');
